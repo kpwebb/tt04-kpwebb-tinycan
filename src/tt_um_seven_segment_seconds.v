f2992bb1aa339fcb319414df700725ee802fe481
kpw <kpwebb@gmail.com>
2023-09-08 13:12:14 -0400

    trying new code

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -1,60 +1,59 @@
 `default_nettype none
 
-module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_seven_segment_seconds (
+    
+    // Module arguments
+    input wire  [7:0] ui_in,
+    output reg  [7:0] uo_out,
+    input wire  [7:0] uio_in,
+    output reg  [7:0] uio_out,
+    output reg  [7:0] uio_oe,
+    input wire  ena,
+    input wire  clk,
+    input wire  rst_n
+    
 );
+    
+    // Stub signals
+    reg  [7:0] my_reg$d;
+    wire  [7:0] my_reg$q;
+    reg  my_reg$clock;
 
-    wire reset = ! rst_n;
-    wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
-
-    // use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
-
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = second_counter[7:0];
-
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
-
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
-
-    always @(posedge clk) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == compare) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
+    
+    // Sub module instances
+    top$my_reg my_reg(
+        .d(my_reg$d),
+        .q(my_reg$q),
+        .clock(my_reg$clock)
+    );
+    
+    // Update code
+    always @(*) begin
+        my_reg$clock = clk;
+        my_reg$d = my_reg$q;
+        my_reg$d = ui_in + uio_in;
+        uo_out = my_reg$q;
+        uio_out = my_reg$q;
+        uio_oe = my_reg$q;
     end
+    
+endmodule // top
 
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
 
-endmodule
+module top$my_reg(d,q,clock);
+    
+    // Module arguments
+    input wire  [7:0] d;
+    output reg  [7:0] q;
+    input wire  clock;
+    
+    // Update code (custom)
+    initial begin
+       q = 8'h0;
+    end
+    
+    always @(posedge clock) begin
+       q <= d;
+    end
+          
+endmodule // top$my_reg
