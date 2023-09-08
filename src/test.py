14fc02ffb9a82617335505f0db89e01f7ad3a4a6
kpw <kpwebb@gmail.com>
2023-09-08 15:24:34 -0400

    switched to adder

--- src/test.py
+++ src/test.py
@@ -2,9 +2,6 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
-
 @cocotb.test()
 async def test_7seg(dut):
     dut._log.info("start")
@@ -15,34 +12,21 @@ async def test_7seg(dut):
     dut._log.info("reset")
     dut.rst_n.value = 0
     # set the compare value
-    dut.ui_in.value = 1
+    dut.ui_in.value = 4
+    dut.uio_in.value = 5
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
 
-        # all bidirectionals are set to output
-        assert dut.uio_oe == 0xFF
+    await ClockCycles(dut.clk, 1000)
 
-    # reset
-    dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    assert int(dut.uo_out.value) == 9
 
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+
+    
+
+
+    
+
+
 
---
+++ src/tt_um_kpwebb_adder.v
@@ -0,0 +1,59 @@
+`default_nettype none
+
+module tt_um_kpwebb_adder (
+    
+    // Module arguments
+    input wire  [7:0] ui_in,
+    output reg  [7:0] uo_out,
+    input wire  [7:0] uio_in,
+    output reg  [7:0] uio_out,
+    output reg  [7:0] uio_oe,
+    input wire  ena,
+    input wire  clk,
+    input wire  rst_n
+    
+);
+    
+    // Stub signals
+    reg  [7:0] my_reg$d;
+    wire  [7:0] my_reg$q;
+    reg  my_reg$clock;
+
+    
+    // Sub module instances
+    top$my_reg my_reg(
+        .d(my_reg$d),
+        .q(my_reg$q),
+        .clock(my_reg$clock)
+    );
+    
+    // Update code
+    always @(*) begin
+        my_reg$clock = clk;
+        my_reg$d = my_reg$q;
+        my_reg$d = ui_in + uio_in;
+        uo_out = my_reg$q;
+        uio_out = my_reg$q;
+        uio_oe = my_reg$q;
+    end
+    
+endmodule // top
+
+
+module top$my_reg(d,q,clock);
+    
+    // Module arguments
+    input wire  [7:0] d;
+    output reg  [7:0] q;
+    input wire  clock;
+    
+    // Update code (custom)
+    initial begin
+       q = 8'h0;
+    end
+    
+    always @(posedge clock) begin
+       q <= d;
+    end
+          
+endmodule // top$my_reg
